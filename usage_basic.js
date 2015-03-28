fdf88a3324f096b722a821a28535db75b14544bd
Ornote <fred.ornote@gmail.com>
2015-03-28 19:29:27 +0100

    Update usage_basic.js

--- usage_basic.js
+++ usage_basic.js
@@ -1,33 +1,34 @@
 ( function( $ ) {
+	Drupal.behaviors.SimpleSlideTouch = {
+    	attach: function (context, settings) {
 
-	$( document ).ready( function() {
+			$( document ).ready( function() {
 
-		$('.slideshow').lightSlider({
-			'oninit' : function($container, el, options, $items, datas, showItem, decalItem){
-				// touch events
-				$container.swipe({
-					swipeLeft:function(event, direction, distance, duration, fingerCount) {
-						decalItem(1);
+				$('.slideshow').lightSlider({
+					'oninit' : function($container, el, options, $items, datas, showItem, decalItem){
+						// touch events
+						$container.swipe({
+							swipeLeft:function(event, direction, distance, duration, fingerCount) {
+								decalItem(1);
+							},
+							swipeRight:function(event, direction, distance, duration, fingerCount) {
+								decalItem(-1);
+							}
+						});
 					},
-					swipeRight:function(event, direction, distance, duration, fingerCount) {
-						decalItem(-1);
+					'onshowitem' : function($container, prev_item, cur_item, options, $items, datas){
+						$($items.get(prev_item)).animate({'margin-left':'100%'}, 700, function(){
+							$(this).css('display','none');
+						});
+						$($items.get(cur_item)).css({'margin-left':'-100%', 'display':'block'}).animate({'margin-left':'0'}, 700, function(){
+							
+						});
+
+						$container.removeClass(options.cantAnimClass);
 					}
 				});
-			},
-			'onshowitem' : function($container, prev_item, cur_item, options, $items, datas){
-				$($items.get(prev_item)).animate({'margin-left':'100%'}, 700, function(){
-					$(this).css('display','none');
-				});
-				$($items.get(cur_item)).css({'margin-left':'-100%', 'display':'block'}).animate({'margin-left':'0'}, 700, function(){
-					
-				});
 
-				$container.removeClass(options.cantAnimClass);
-			}
-		});
-
-	} );
-
+	 		} );
+        }
+    };
 } )( jQuery );
-
-
