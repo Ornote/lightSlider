adb68ef44ae573f1b7822b4cba8c99c0d7014afc
Ornote <fred.ornote@gmail.com>
2015-03-28 19:30:41 +0100

    Update usage_basic.js

--- usage_basic.js
+++ usage_basic.js
@@ -1,6 +1,6 @@
 ( function( $ ) {
-	Drupal.behaviors.SimpleSlideTouch = {
-    	attach: function (context, settings) {
+	Drupal.behaviors.lightSliderTouch = {
+    		attach: function (context, settings) {
 
 			$( document ).ready( function() {
 
